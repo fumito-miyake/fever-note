81656aff87c4381041c46f68fb06b70770f806d2
fumy <32849561+fumito-miyake@users.noreply.github.com>
2026-02-12 23:02:41 +0900

    feat: create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "fever-note-pwa-cache-v1";
+const urlsToCache = [
+  "/fever-note/",
+  "/fever-note/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
